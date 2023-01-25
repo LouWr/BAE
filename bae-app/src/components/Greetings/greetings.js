fd6e86e3d50c5343b8d98b3fe7678e51fef3f897
Callum <84125856+callum-cheshire@users.noreply.github.com>
2023-01-25 14:28:50 +0000

    modified navbar

---
+++ bae-app/src/components/Greetings/greetings.js
@@ -0,0 +1,9 @@
+const Greetings = () => {
+  return (
+    <div>
+      <h1>Greetings, how are you today?</h1>
+    </div>
+  );
+};
+
+export default Greetings;
--- bae-app/src/components/NavBar/NavBar.js
+++ bae-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,33 @@
-import { Container, Flex, Spacer } from "@chakra-ui/react";
+import { Container, Flex, Spacer, Wrap, WrapItem } from "@chakra-ui/react";
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
+import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
+import logo from "../../assets/EcoPointsLogo.png";
 
-export const NavBar = () => {
+import "./NavBar.css";
+
+const NavBar = () => {
   return (
-    <Container maxW={"100%"}>
-      <Flex align="center">
-        <h1>NavBar</h1>
-        <Spacer h={"80px"} />
+    <Container
+      maxW={"100%"}
+      className="navContainer"
+      // align="center"
+      // minWidth="max-content"
+      // align-items="center"
+      // gap="2"
+    >
+      <Flex>
+        <Spacer h={"20px"} />
+        <img className="logo" src={logo} alt="" />
+        <Wrap>
+          <WrapItem>
+            <Avatar gap="2" name="avatar" src="" />
+          </WrapItem>
+        </Wrap>
+        <Spacer h={"20px"} />
         <BurgerMenu />
       </Flex>
     </Container>
   );
 };
+
+export default NavBar;
