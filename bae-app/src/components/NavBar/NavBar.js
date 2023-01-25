16be7824a40e9e637a13a7ce8a751ddbc6d4beeb
Francesca Croutear-Foy <crofoys@gmail.com>
2023-01-25 15:04:45 +0000

    added list and checkbox

--- bae-app/src/components/NavBar/NavBar.js
+++ bae-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,16 @@
 import { Container, Flex, Spacer } from "@chakra-ui/react";
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
+import logo from "../../EcoPointsLogo.png";
+import "./NavBar.css";
 
 export const NavBar = () => {
-  return (
-    <Container maxW={"100%"}>
-      <Flex align="center">
-        <h1>NavBar</h1>
-        <Spacer h={"80px"} />
-        <BurgerMenu />
-      </Flex>
-    </Container>
-  );
+	return (
+		<Container className="navContainer" maxW={"100%"}>
+			<Flex align="center">
+				<img className="logo" src={logo} alt="" />
+				<Spacer h={"80px"} />
+				<BurgerMenu />
+			</Flex>
+		</Container>
+	);
 };
